cad0eec206dbb04327d4e3bfe97cc23d8ae081e7
Arshad410 <arshadsalim00@gmail.com>
2020-06-29 13:17:12 +0530

    Update Db.js

--- config/Db.js
+++ config/Db.js
@@ -1,5 +1,5 @@
 // db connection string
 
 module.exports = {
-    DB: "mongodb+srv://new_user:arsh1234@cluster0-rvkdl.mongodb.net/DB-CRUD?retryWrites=true&w=majority"
+    DB: "mongodb+srv://new_user:arsh1234@cluster0-rvkdl.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
 }
